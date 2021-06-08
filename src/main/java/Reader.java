5fba17e4a99ad2738bd48c85dfd324c297c70a53
hollyhbrady <hollyhbrady@gmail.com>
2021-06-08 18:31:07 +0100

    Reader class added

---
+++ src/main/java/Reader.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Reader {
+
+    private ArrayList<Book> book;
+    private String name;
+    private int reading;
+
+    public Reader(String name, int reading) {
+        this.name = name;
+        this.reading = 0;
+    }
+
+}
