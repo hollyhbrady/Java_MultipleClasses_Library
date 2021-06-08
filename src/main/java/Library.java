58069f60af709284702c632c95d4142928b370b8
hollyhbrady <hollyhbrady@gmail.com>
2021-06-08 17:54:15 +0100

    Library has Book ArrayList and capacity

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,11 @@
 public class Library {
+
+    private ArrayList<Book> books;
+    privte int capacity;
+
+    public Library(int capacity) {
+        this.capacity = capacity;
+        this.books = new ArrayList<Book>();
+    }
+
 }
